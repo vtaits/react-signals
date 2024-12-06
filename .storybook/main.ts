e205e28a9db0b41a0b5ab15b77572432f2ae1c3b
Vadim Taits <taizvadim@gmail.com>
2024-12-06 13:57:08 +0300

    Upgrade dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,11 +13,9 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-coverage"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
